79b3da4522fe386974d346813a4aba48893e7067
Gilsonlgd <garciadasilvagilson@gmail.com>
2023-05-30 02:50:49 -0300

    add api domain

--- Frontend/my-react-app/src/Services/api.js
+++ Frontend/my-react-app/src/Services/api.js
@@ -5,7 +5,7 @@ import store from "./store";
 const isProd = process.env.NODE_ENV === "production";
 
 const apiUrl = isProd
-  ? "http://127.0.0.1:3333"
+  ? "https://terapia.herokuapp.com/"
   : "http://127.0.0.1:3333";
 
 const api = axios.create({
