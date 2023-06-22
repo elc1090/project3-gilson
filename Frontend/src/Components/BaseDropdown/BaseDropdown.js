3edf2e2df0142dd494b169e17e8cefd4629e5797
Gilsonlgd <garciadasilvagilson@gmail.com>
2023-06-22 13:45:25 -0300

    final updates on baseDropdown

--- Frontend/src/Components/BaseDropdown/BaseDropdown.js
+++ Frontend/src/Components/BaseDropdown/BaseDropdown.js
@@ -30,33 +30,44 @@ function BaseDropdown({ value, setValue, label, icon, children }) {
 
   return (
     <div className={`select-group field`} ref={dropdownRef}>
-      <select
-        className={`select-field ${noneSelection ? "none-selection" : ""}`}
-        name={label}
-        id={label}
-        value={value}
-        onChange={handleInputChange}
-        readOnly
-        required
+      <div
+        className={`select-field ${!isDropdownOpen ? "dropdown-closed" : ""} ${
+          noneSelection ? "none-selection" : ""
+        }`}
         onClick={toggleDropdown}
       >
-        <option defaultValue={""} disabled></option>
-        {React.Children.map(children, (child) => (
-          <option key={child.props.value} value={child.props.value}>
-            {child.props.value}
-          </option>
-        ))}
-      </select>
+        <span className="selected-option">
+          {value || ""}
+        </span>
+        <i
+          className={`fas fa-arrow-down toggle-dropdown ${
+            isDropdownOpen ? "open" : ""
+          }`}
+        ></i>
+      </div>
+
+      {isDropdownOpen && (
+        <ul className="menu-dropdown">
+          {React.Children.map(children, (child) => (
+            <li
+              className="dropdown-item"
+              key={child.props.value}
+              onClick={() => {
+                setValue(child.props.value);
+                setIsDropdownOpen(false);
+                setNoneSelection(false);
+              }}
+            >
+              {child.props.value}
+            </li>
+          ))}
+        </ul>
+      )}
+
       <label htmlFor={label} className="select-label">
         {" "}
         <i className={icon}></i> {label}
       </label>
-      <i
-        className={`fas fa-arrow-down toggle-dropdown ${
-          isDropdownOpen ? "open" : ""
-        }`}
-        onClick={toggleDropdown}
-      ></i>
     </div>
   );
 }
