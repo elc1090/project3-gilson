fff63c49dc96c9e8f7caf6bca30af9d59abb8d6b
Gilsonlgd <garciadasilvagilson@gmail.com>
2023-06-26 22:11:46 -0300

    updates on new session page

--- Frontend/src/Views/Psychologist/Sessions/NewSession.js
+++ Frontend/src/Views/Psychologist/Sessions/NewSession.js
@@ -7,6 +7,18 @@ import { useStopwatch } from "react-timer-hook";
 import BaseButton from "../../../Components/BaseButton/BaseButton";
 import IconButton from "../../../Components/IconButton/IconButton";
 import StyledInput from "../../../Components/StyledInput/StyledInput";
+import Semaphore from "../../../Components/SemaphoreInput/SemaphoreInput";
+
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import Checkbox from "@mui/material/Checkbox";
+
+import { Tooltip } from "react-tooltip";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 
@@ -14,9 +26,18 @@ const NewSession = () => {
   const navigate = useNavigate();
   const [changingHeader] = useState(false);
   const [patient, setPatient] = useState({ name: "Gilson Garcia" });
+  const { seconds, minutes, hours } = useStopwatch({ autoStart: true });
+  const [rows, setRows] = useState([]);
+
   const [formData, setFormData] = useState({
     general_notes: "",
     diagnosis: "",
+  });
+
+  const [demmandModel, setDemmandModel] = useState({
+    check: false,
+    title: "",
+    level: "",
   });
 
   function getHeaderTextClass() {
@@ -24,9 +45,48 @@ const NewSession = () => {
     else return "text-enter";
   }
 
-  const handleChange = (name, value) => {
+  function getSemaphoreColor(level) {
+    switch (level) {
+      case "low":
+        return "green";
+      case "medium":
+        return "yellow";
+      case "high":
+        return "red";
+      default:
+        return "";
+    }
+  }
+
+  const handleQuillChanges = (name, value) => {
     setFormData((prevData) => ({ ...prevData, [name]: value }));
   };
+
+  const handleDemmandChange = (name, value) => {
+    setDemmandModel((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleCheckboxChange = (index) => {
+    const updatedRows = [...rows];
+    updatedRows[index] = {
+      ...updatedRows[index],
+      check: !updatedRows[index].check,
+      updated: true,
+    };
+    setRows(updatedRows);
+  };
+
+  function handleAddDemmand() {
+    const updatedRows = [...rows];
+    updatedRows.push({
+      check: false,
+      title: demmandModel.title,
+      level: demmandModel.level,
+      updated: true,
+    });
+    setRows(updatedRows);
+    setDemmandModel({ check: false, title: "", level: "" });
+  }
 
   function handleFinishSession() {
     navigate(`/psychologist/sessions`);
@@ -34,8 +94,6 @@ const NewSession = () => {
   function handleCancelSession() {
     navigate(`/psychologist/sessions`);
   }
-
-  const { seconds, minutes, hours } = useStopwatch({ autoStart: true });
 
   return (
     <div className="new-session-container">
@@ -82,7 +140,7 @@ const NewSession = () => {
 
       <div className="page-body">
         <div className="d-flex row justify-content-between">
-          <div className="col-lg-4">
+          <div className="col-lg-6">
             <div className="row">
               <div className="card p-0">
                 <div className="card-header">
@@ -91,7 +149,9 @@ const NewSession = () => {
                 <div className="card-body">
                   <ReactQuill
                     value={formData.general_notes}
-                    onChange={(value) => handleChange("general_notes", value)}
+                    onChange={(value) =>
+                      handleQuillChanges("general_notes", value)
+                    }
                     key={"1_react_quill"}
                   />
                 </div>
@@ -105,32 +165,121 @@ const NewSession = () => {
                 <div className="card-body">
                   <ReactQuill
                     value={formData.diagnosis}
-                    onChange={(value) => handleChange("diagnosis", value)}
+                    onChange={(value) => handleQuillChanges("diagnosis", value)}
                     key={"2_react_quill"}
                   />
                 </div>
               </div>
             </div>
           </div>
-          <div className="col-lg-4">
+          <div className="col-lg-6 demand-column">
             <div className="row h-100">
               <div className="full-height card p-0">
                 <div className="card-header">
                   <h3 className="card-title">Demandas</h3>
                 </div>
-                <div className="card-body"></div>
+                <div className="card-body">
+                  {rows.length ? (
+                    <TableContainer sx={{ borderRadius: 0 }} component={Paper}>
+                      <Table aria-label="simple table">
+                        <TableHead>
+                          <TableRow>
+                            <TableCell
+                              padding={"none"}
+                              sx={{ maxWidth: "10px" }}
+                              align="left"
+                            >
+                              <Checkbox disabled />
+                            </TableCell>
+                            <TableCell padding={"none"}>Título</TableCell>
+                            <TableCell padding={"none"} align="left">
+                              Relevância
+                            </TableCell>
+                          </TableRow>
+                        </TableHead>
+                        <TableBody>
+                          {rows.map((row, index) => (
+                            <TableRow
+                              key={row.title}
+                              sx={{
+                                "&:last-child td, &:last-child th": {
+                                  border: 0,
+                                },
+                              }}
+                            >
+                              <TableCell width={"45px"} padding={"none"}>
+                                <Checkbox
+                                  checked={row.check}
+                                  onChange={() => handleCheckboxChange(index)}
+                                />
+                              </TableCell>
+                              <TableCell
+                                padding={"none"}
+                                component="th"
+                                scope="row"
+                              >
+                                {row.title}
+                              </TableCell>
+                              <TableCell
+                                width={"20%"}
+                                padding={"none"}
+                                align="left"
+                              >
+                                <div className="d-flex align-items-center">
+                                  <div
+                                    className={`circle ${getSemaphoreColor(
+                                      row.level
+                                    )} selected`}
+                                  />
+                                  {(row.level === "low" && "Baixa") ||
+                                    (row.level === "medium" && "Média") ||
+                                    (row.level === "high" && "Alta")}
+                                </div>
+                              </TableCell>
+                            </TableRow>
+                          ))}
+                        </TableBody>
+                      </Table>
+                    </TableContainer>
+                  ) : (
+                    <div className="d-flex justify-content-center align-items-center h-100">
+                      <span className="text-muted">
+                        Nenhuma demanda adicionada
+                      </span>
+                    </div>
+                  )}
+                </div>
                 <div className="card-footer">
-                  <div className="d-flex align-items-center justify-content-center">
+                  <div className="d-flex footer-container align-items-center justify-content-center">
                     <div className="input-sm-container margin-right-sm">
                       <StyledInput
+                        setValue={(value) =>
+                          handleDemmandChange("title", value)
+                        }
                         type="primary-black"
                         placeholder={"Nova demanda"}
                         labelType={"insideLabel"}
-                        icon={"fas fa-clock"}
+                        icon={"fas fa-boxes-stacked"}
                       ></StyledInput>
                     </div>
+                    <div className="semaphore-container">
+                      <Semaphore
+                        selectedLevel={demmandModel.level}
+                        onChange={(value) =>
+                          handleDemmandChange("level", value)
+                        }
+                      />
+                    </div>
                     <div>
-                      <IconButton icon={"fas fa-plus"} iconClass={"icon-md"} />
+                      <IconButton
+                        id="add-demand-btn"
+                        icon={"fas fa-plus"}
+                        iconClass={"icon-xl"}
+                        onClick={handleAddDemmand}
+                      />
+                      <Tooltip anchorSelect="#add-demand-btn" place="top">
+                        Adicionar Demanda
+                      </Tooltip>
                     </div>
                   </div>
                 </div>
